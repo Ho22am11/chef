ff250132873cb690b5f03ea4e451d4c9963cb00e
hossam <harrazhossam11@gmail.com>
2024-10-27 17:24:36 +0300

    register admin

---
+++ app/Http/Controllers/Admin/AuthController.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Models\Admin;
+use Dotenv\Validator as DotenvValidator;
+use Illuminate\Contracts\Validation\Validator as ValidationValidator;
+use Validator;
+class AuthController extends Controller
+{
+    public function register(Request $request){
+        
+        try{
+            $validator = Validator::make($request->all(), [
+                'name' => 'required|string|between:2,100',
+                'email' => 'required|string|email|max:100|unique:admins',
+                'password' => 'required|string|min:6',
+            ]);
+        
+            if ($validator->fails()) { // if exist error in validation return it
+                return response()->json($validator->errors(), 400);
+            }
+        
+            $admin = Admin::create([
+                'name' => $request->name,
+                'email' => $request->email,
+                'password' => bcrypt($request->password),
+            ]);
+            
+    
+            $token = auth()->guard('admin')->attempt($request->only('email', 'password'));
+            $admin->token = $token ;
+    
+            if (!$token) {
+                return response()->json(['error' => 'Unauthorized'], 401);
+            }
+        
+            return $admin ;
+
+        }catch(\Exception $e){
+            return response()->json([
+            'error' => 'Something went wrong',
+            'message' => $e->getMessage()], 500);
+        }
+
+    }
+
+}
--- routes/admin.php
+++ routes/admin.php
@@ -2,9 +2,11 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Admin\AuthController;
 
 
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+
+Route::prefix('auth')->group(function(){
+    Route::post('/register' , [ AuthController::class , 'register']);
 });
