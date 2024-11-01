02d5ea5a2927351b661cb90dc2f04ab262d04ca3
hossam <harrazhossam11@gmail.com>
2024-11-01 14:48:01 +0200

    build services

---
+++ app/Http/Controllers/Admin/ServiceController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use App\Models\Service;
+use Illuminate\Http\Request;
+use App\Traits\ApiResponseTrait;
+
+
+class ServiceController extends Controller
+{
+    use ApiResponseTrait ;
+    public function index(){
+        $services = Service::all();
+        return $this->ApiResponse($services , 'get all services successfully' , 200 ) ;
+    }
+
+    public function show($id){
+        $service = Service::findOrFail($id);
+        return $this->ApiResponse($service , 'get service has'.$id.' successfully' , 200 ) ;
+
+    }
+
+    public function store(Request $request){
+        $validatedData  = $request->validate(['name' => 'required|string|between:2,20']);
+        $service = Service::create($validatedData);
+        return $this->ApiResponse( $service , 'store service  successfully' , 201 ) ;
+
+    }
+    public function update(Request $request , $id){
+        $validatedData  = $request->validate(['name' => 'required|string|between:2,20']);
+        $service = Service::findOrFail($id);
+        $service->update($validatedData);
+        return $this->ApiResponse( $service , 'update service  successfully' , 200 ) ;
+    }
+
+    public function destroy($id){
+        Service::destroy($id);
+        return $this->ApiResponse(null, 'delete service  successfully' , 204 ) ;
+
+    }
+
+}
--- routes/admin.php
+++ routes/admin.php
@@ -3,14 +3,12 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Admin\AuthController;
-
-
-
+use App\Http\Controllers\Admin\ServiceController;
 
 Route::prefix('auth')->group(function(){
     Route::post('/register' , [ AuthController::class , 'register']);
 });
 
 Route::middleware(['api','auth.guard:admin'])->group(function(){
-    // all routes
+    Route::resource('services' , ServiceController::class )->except(['create', 'edit']);
 }); 
